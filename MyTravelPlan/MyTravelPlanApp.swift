b2f0a6868696b770d8b4bfbedc0771796d6a947b
Christophe <c.milliere>
2023-04-10 18:39:48 +0200

    Initial Commit

---
+++ MyTravelPlan/MyTravelPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTravelPlanApp.swift
+//  MyTravelPlan
+//
+//  Created by Christophe on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyTravelPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
